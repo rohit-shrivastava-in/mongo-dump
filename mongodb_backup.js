86b14f29057b17c972c634f786d32df60a65a187
rohit_ajrh <rohit.shri2389@gmail.com>
2017-11-29 17:18:10 +0530

    module created

---
+++ mongodb_backup.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+var child_process = require('child_process');
+
+// BD options setup
+var dbOptions =  {
+    user: false,
+    pass: false,
+    host: 'localhost',
+    port: 27017,
+    database: 'Interio',
+    autoBackup: true, 
+    removeOldBackup: true,
+    keepLastDaysBackup: 2,
+    autoBackupPath: 'F:/auto-backup/' // i.e. /var/database-backup/
+};
+
+/* return date object */
+exports.stringToDate = (dateString) => {
+    return new Date(dateString);
+}
+
+// Auto backup script
+exports.dbAutoBackUp= () => {
+    console.log("taking backup")
+// check for auto backup is enabled or disabled
+    if (dbOptions.autoBackup == true) {
+        var date = new Date();
+        var beforeDate, oldBackupDir, oldBackupPath;
+        currentDate = this.stringToDate(date); // Current date
+        var newBackupDir = currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1) + '-' + currentDate.getDate();
+        var newBackupPath = dbOptions.autoBackupPath + 'mongodump-' + newBackupDir; // New backup path for current backup process
+        // check for remove old backup after keeping # of days given in configuration
+        if (dbOptions.removeOldBackup == true) {
+            beforeDate = currentDate;
+            beforeDate.setDate(beforeDate.getDate() - dbOptions.keepLastDaysBackup); // Substract number of days to keep backup and remove old backup
+            oldBackupDir = beforeDate.getFullYear() + '-' + (beforeDate.getMonth() + 1) + '-' + beforeDate.getDate();
+            oldBackupPath = dbOptions.autoBackupPath + 'mongodump-' + oldBackupDir; // old backup(after keeping # of days)
+        }
+
+        var cmd = 'mongodump --host ' + dbOptions.host + ' --port ' + dbOptions.port + ' --db ' + dbOptions.database + ' --out ' + newBackupPath; // Command for mongodb dump process
+        child_process.exec(cmd, function (error, stdout, stderr) {
+            
+            if(error)
+                console.log("Error", error)
+
+            // check for remove old backup after keeping # of days given in configuration
+            if (dbOptions.removeOldBackup == true) 
+                if (fs.existsSync(oldBackupPath)) {
+                    child_process.exec("rm -rf " + oldBackupPath, function (err) { });
+                }
+        });
+    }
+}
--- server.js
+++ server.js
@@ -1,24 +1,9 @@
+var CronJob = require('cron').CronJob;
+var Cron = require('./mongodb_backup.js');
 
-
-const fs = require('fs');
-const child_process = require('child_process');
-var date=new Date().getDate();
-var month=new Date().getMonth();
-var year=new Date().getFullYear();
-var output='F:/auto-backup/';
-child_process.exec('mongodump --db Interio --out ' + output + year+'-'+month+'-'+date, function (err, res) {
-    console.log('Dump taken on '+ year+'-'+month+'-'+date)
-    console.log(output)
-    fs.readdir(output,function(error,files){
-        // console.log("files", files);
-        // console.log("error", error);
-        files.forEach(function(file){
-            console.log(file);
-            if(file.match(date)){
-                console.log(date-1)
-                // deleteFolderRecursive('./DumpMaster/'+file);
-            }
-        })
-    })
-   
-});
+new CronJob('* * * * * *', () => {
+    Cron.dbAutoBackUp();
+}, () => {
+    //Will execute when job will stop
+    console.log("stopped")
+}, true, 'Asia/Kolkata');
